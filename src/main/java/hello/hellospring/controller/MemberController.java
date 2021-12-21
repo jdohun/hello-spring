0cd8c61cd78e19c03e554d27bfe25f1fe76a4224
jdohun <wjdehgns97@naver.com>
2021-12-21 18:41:36 +0900

    16 ~ 17강 회원 웹 기능 - 홈 화면 추가, 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // 어노테이션을 통해서 Spring이 맨처음 작동될때 Spring Container라는 통이 생기는데 거기에 객체를 미리 생성해서 넣어두고 Spring이 관리함
 @Controller
@@ -12,5 +15,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
