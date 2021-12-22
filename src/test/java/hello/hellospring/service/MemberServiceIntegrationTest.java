3191cbf92ef4f018b9a17e90df4388149eacdd79
jdohun <wjdehgns97@naver.com>
2021-12-22 15:51:52 +0900

    21강 스프링 DB접근 기술 - 스프링 통합 테스트

---
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,46 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository MemberRepository;
+
+    @Test
+    void 회원가입() {   // Test 코드는 한글로 적어도 상관 없다.
+        //given
+        Member member  = new Member();
+        member.setName("spring");
+
+        //when
+        Long SaveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(SaveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, ()->memberService.join(member2));
+    }
+}
