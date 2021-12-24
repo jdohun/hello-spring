8fe68759353dbd022f04f3167bc740b92aab83a4
jdohun <wjdehgns97@naver.com>
2021-12-24 15:28:40 +0900

    24강 스프링 DB접근 기술 - 스프링 데이터 JPA // 이전 강의 밀린거 아님 이게 24강임

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -13,30 +13,23 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private  EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired // 생성자가 하나인 경우는 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-/* // Spring에서 생성
-    private DataSource dataSource;
-
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }*/
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+/*    @Bean
     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+    }*/
 }
