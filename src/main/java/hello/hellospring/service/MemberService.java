3dc7d50613b77c6bd5a6fc79ba373d94332067e1
jdohun <wjdehgns97@naver.com>
2021-12-24 16:52:43 +0900

    27강 AOP - AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -20,20 +20,12 @@ public class MemberService {
     * 회원 가입
     **/
     public  Long join(Member member){
-        long start = System.currentTimeMillis();
-        try{
-            // 같은 이름이 있는 중복 회원X
-            // result != null 이면 아래 문장이 동작함
-            // Optional 이기 때문에 가능함
-            validateDuplicateMember(member); // 중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
-
+        // 같은 이름이 있는 중복 회원X
+        // result != null 이면 아래 문장이 동작함
+        // Optional 이기 때문에 가능함
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -47,14 +39,7 @@ public class MemberService {
     * 전체 회원 조회
     **/
     public List<Member> findMembers(){
-        long start = System.currentTimeMillis();
-        try{
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
